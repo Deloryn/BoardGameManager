c75f993c9cf3a44272aaf195945dda287899feac
deloryn <krzysztof.michalak96@gmail.com>
2019-02-16 22:26:04 +0100

    Add tournamentId in TournamentReservation

--- src/main/java/pl/put/boardgamemanager/reservation/tournament_reservation/TournamentReservation.java
+++ src/main/java/pl/put/boardgamemanager/reservation/tournament_reservation/TournamentReservation.java
@@ -10,13 +10,24 @@ import java.util.Objects;
 @DiscriminatorValue("TournamentReservation")
 public class TournamentReservation extends Reservation {
 
+    @Column(name = "tournamentid", nullable = false, unique = true)
+    private Long tournamentId;
+
+    public Long getTournamentId() {
+        return tournamentId;
+    }
+
+    public void setTournamentId(Long tournamentId) {
+        this.tournamentId = tournamentId;
+    }
+
     @Override
     public boolean equals(Object o) {
         if(!super.equals(o)) return false;
         else if(getClass() != o.getClass()) return false;
         else {
             TournamentReservation that = (TournamentReservation) o;
-            return Objects.equals(tutorId, that.tutorId);
+            return Objects.equals(tournamentId, that.tournamentId);
         }
     }
 
