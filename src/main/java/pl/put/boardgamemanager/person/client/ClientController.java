5d302adccbb069339e7378a02dc1c5a7a695879a
deloryn <krzysztof.michalak96@gmail.com>
2019-02-17 15:42:18 +0100

    Create ClientController and ClientDTO

---
+++ src/main/java/pl/put/boardgamemanager/person/client/ClientController.java
@@ -0,0 +1,8 @@
+package pl.put.boardgamemanager.person.client;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientController {
+
+}
---
+++ src/main/java/pl/put/boardgamemanager/person/client/ClientDTO.java
@@ -0,0 +1,4 @@
+package pl.put.boardgamemanager.person.client;
+
+public class ClientDTO {
+}
