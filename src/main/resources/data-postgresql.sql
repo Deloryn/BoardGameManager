d484c0951f8f909fc63f7ca61a6642e373e2e6e6
deloryn <krzysztof.michalak96@gmail.com>
2019-02-17 13:37:28 +0100

    Fill data-postgresql.sql

--- src/main/resources/data-postgresql.sql
+++ src/main/resources/data-postgresql.sql
@@ -1,1 +1,3 @@
 -- Here we can place initial date to insert into database
+INSERT INTO Persons(id, name, surname, email, phoneNumber, role) VALUES (1, 'Jan', 'Kowalski', 'jan.kowalski@poczta.pl', 'c');
+COMMIT;
