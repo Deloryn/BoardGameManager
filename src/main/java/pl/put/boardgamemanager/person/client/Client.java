84320fd5f7ab2fbc634fcdd0823b4c64301576f5
deloryn <krzysztof.michalak96@gmail.com>
2019-02-17 18:33:58 +0100

    Add fromDTO and toDTO in Client.java

--- src/main/java/pl/put/boardgamemanager/person/client/Client.java
+++ src/main/java/pl/put/boardgamemanager/person/client/Client.java
@@ -9,4 +9,28 @@ import javax.persistence.*;
 @DiscriminatorValue("c")
 public class Client extends Person {
 
+    public static Client fromDTO(ClientDTO dto) {
+        Client client = new Client();
+
+        client.setId(dto.getId());
+        client.setName(dto.getName());
+        client.setSurname(dto.getSurname());
+        client.setEmail(dto.getEmail());
+        client.setPhoneNumber(dto.getPhoneNumber());
+
+        return client;
+    }
+
+    public static ClientDTO toDTO(Client client) {
+        ClientDTO dto = new ClientDTO();
+
+        dto.setId(client.getId());
+        dto.setName(client.getName());
+        dto.setSurname(client.getSurname());
+        dto.setEmail(client.getEmail());
+        dto.setPhoneNumber(client.getPhoneNumber());
+
+        return dto;
+    }
+
 }
