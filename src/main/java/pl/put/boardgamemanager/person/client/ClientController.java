10e94bbb1f87668173c562744aafb3bf0a9fd880
deloryn <krzysztof.michalak96@gmail.com>
2019-02-17 15:56:15 +0100

    Add ClientRepository to ClientController

--- src/main/java/pl/put/boardgamemanager/person/client/ClientController.java
+++ src/main/java/pl/put/boardgamemanager/person/client/ClientController.java
@@ -1,8 +1,12 @@
 package pl.put.boardgamemanager.person.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientController {
+
+    @Autowired
+    private ClientRepository clientRepository;
 
 }
