4d2908cbbcc1abf035807736a3feca591ed2c2a4
deloryn <krzysztof.michalak96@gmail.com>
2019-02-17 18:35:00 +0100

    Remove unnecessary method from ClientRepository

--- src/main/java/pl/put/boardgamemanager/person/client/ClientRepository.java
+++ src/main/java/pl/put/boardgamemanager/person/client/ClientRepository.java
@@ -2,8 +2,6 @@ package pl.put.boardgamemanager.person.client;
 
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface ClientRepository extends CrudRepository<Client, Long> {
-    List<Client> findByName(String name);
+
 }
