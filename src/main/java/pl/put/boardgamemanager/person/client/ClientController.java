12390b29399373c2f96e1d288fd88c46c2d4f428
deloryn <krzysztof.michalak96@gmail.com>
2019-02-17 18:09:51 +0100

    Add CRUDL methods to ClientController

--- src/main/java/pl/put/boardgamemanager/person/client/ClientController.java
+++ src/main/java/pl/put/boardgamemanager/person/client/ClientController.java
@@ -2,23 +2,58 @@ package pl.put.boardgamemanager.person.client;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ClientController {
 
     @Autowired
-    private ClientRepository clientRepository;
+    private ClientRepository repository;
 
     @GetMapping("/clients/{id}")
-    public ClientDTO getClient(@PathVariable String id) {
-        return mapClientToDTO(clientRepository.findByName(id).get(0));
+    public ClientDTO get(@PathVariable Long id) {
+        Client client = repository.findById(id).orElse(null);
+        return mapClientToDTO(client);
     }
+
+    @GetMapping("/clients")
+    public List<Client> all() {
+        return (List<Client>) repository.findAll();
+    }
+
+    @PostMapping("/clients")
+    public void create(@RequestBody Client client) {
+        repository.save(client);
+    }
+
+    @PutMapping("/clients/{id}")
+    public ClientDTO update(@RequestBody Client newClient, @PathVariable Long id) {
+        return repository.findById(id)
+                .map(client -> {
+                    client.setName(newClient.getName());
+                    client.setSurname(newClient.getSurname());
+                    client.setEmail(newClient.getEmail());
+                    client.setPhoneNumber(newClient.getPhoneNumber());
+                    repository.save(client);
+                    return mapClientToDTO(client);
+                })
+                .orElseGet(() -> {
+                    repository.save(newClient);
+                    return mapClientToDTO(newClient);
+                });
+    }
+
+    @DeleteMapping("/clients/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+
 
     private ClientDTO mapClientToDTO(Client client) {
         ModelMapper modelMapper = new ModelMapper();
         return modelMapper.map(client, ClientDTO.class);
     }
+
 }
