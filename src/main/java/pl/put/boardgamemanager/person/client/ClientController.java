0f5c9e134cf6736d28f689f8c33a444a6524d932
deloryn <krzysztof.michalak96@gmail.com>
2019-02-17 17:02:53 +0100

    Add getClient(id) to ClientController

--- src/main/java/pl/put/boardgamemanager/person/client/ClientController.java
+++ src/main/java/pl/put/boardgamemanager/person/client/ClientController.java
@@ -2,6 +2,8 @@ package pl.put.boardgamemanager.person.client;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,11 @@ public class ClientController {
 
     @Autowired
     private ClientRepository clientRepository;
+
+    @GetMapping("/clients/{id}")
+    public ClientDTO getClient(@PathVariable String id) {
+        return mapClientToDTO(clientRepository.findByName(id).get(0));
+    }
 
     private ClientDTO mapClientToDTO(Client client) {
         ModelMapper modelMapper = new ModelMapper();
