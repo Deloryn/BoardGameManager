43840397df11f244d3e8d23f23fa4894cb67ed32
deloryn <krzysztof.michalak96@gmail.com>
2019-02-17 16:01:25 +0100

    Implement mapClientToDto(client)

--- src/main/java/pl/put/boardgamemanager/person/client/ClientController.java
+++ src/main/java/pl/put/boardgamemanager/person/client/ClientController.java
@@ -1,5 +1,6 @@
 package pl.put.boardgamemanager.person.client;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +10,8 @@ public class ClientController {
     @Autowired
     private ClientRepository clientRepository;
 
+    private ClientDTO mapClientToDTO(Client client) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(client, ClientDTO.class);
+    }
 }
