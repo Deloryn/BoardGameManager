a2681b5bac179033d9d706189abcc070bb86d604
deloryn <krzysztof.michalak96@gmail.com>
2019-02-18 15:46:35 +0100

    Separate logic in Tutor

--- src/main/java/pl/put/boardgamemanager/rental/private_rental/PrivateRentalController.java
+++ src/main/java/pl/put/boardgamemanager/rental/private_rental/PrivateRentalController.java
@@ -4,49 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class PrivateRentalController {
 
     @Autowired
-    private PrivateRentalRepository repository;
+    private PrivateRentalService service;
 
     @GetMapping("/private_rentals/{id}")
     public PrivateRentalDTO get(@PathVariable Long id) {
-        PrivateRental privateRental = repository.findById(id).orElse(null);
-        return PrivateRental.toDTO(privateRental);
+        return service.get(id);
     }
 
     @GetMapping("/private_rentals")
     public List<PrivateRentalDTO> all() {
-        return repository.findAll().stream()
-                .map(PrivateRental::toDTO)
-                .collect(Collectors.toList());
+        return service.all();
     }
 
     @PostMapping("/private_rentals")
     public void create(@RequestBody PrivateRentalDTO privateRentalDTO) {
-        repository.save(PrivateRental.fromDTO(privateRentalDTO));
+        service.create(privateRentalDTO);
     }
 
     @PutMapping("/private_rentals")
     public PrivateRentalDTO update(@RequestBody PrivateRentalDTO privateRentalDTO) {
-        PrivateRental privateRental = PrivateRental.fromDTO(privateRentalDTO);
-        return repository.findById(privateRental.getCopyId())
-                .map(currentPrivateRental -> {
-                    currentPrivateRental.updateParams(privateRental);
-                    repository.save(currentPrivateRental);
-                    return PrivateRental.toDTO(currentPrivateRental);
-                })
-                .orElseGet(() -> {
-                    repository.save(privateRental);
-                    return PrivateRental.toDTO(privateRental);
-                });
+        return service.update(privateRentalDTO);
     }
 
     @DeleteMapping("/private_rentals/{id}")
     public void delete(@PathVariable Long id) {
-        repository.deleteById(id);
+        service.delete(id);
     }
 }
