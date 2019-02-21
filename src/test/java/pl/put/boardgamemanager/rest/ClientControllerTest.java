7643e828b3cfcaddcc76dd83cef9fe0efd2730bd
EwaFengler <ewa.fen@interia.pl>
2019-02-21 19:52:38 +0100

    add base test example

---
+++ src/test/java/pl/put/boardgamemanager/rest/ClientControllerTest.java
@@ -0,0 +1,189 @@
+package pl.put.boardgamemanager.rest;
+
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ClientControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
+
+    @Test
+    public void should_fiend_all_clients() {
+        given()
+                .header("Accept-Encoding", "application/json")
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .log().all()
+                .when().get("/clients")
+                .then().log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("$", hasSize(1))
+                .body("[0].id", notNullValue())
+                .body("[0].id", equalTo(1))
+                .body("[0].name", equalTo("Jan"))
+                .body("[0].surname", equalTo("Kowalski"))
+                .body("[0].email", equalTo("jan.kowalski@poczta.pl"))
+                .body("[0].phoneNumber", equalTo("123456789"))
+        ;
+    }
+
+    @Test
+    public void should_get_client_by_id() {
+
+        //1, 'Jan', 'Kowalski', 'jan.kowalski@poczta.pl', '123456789'
+        given()
+                .header("Accept-Encoding", "application/json")
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .log().all()
+                .when().get("/clients/1")
+                .then().log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id", notNullValue())
+                .body("id", equalTo(1))
+                .body("name", equalTo("Jan"))
+                .body("surname", equalTo("Kowalski"))
+                .body("email", equalTo("jan.kowalski@poczta.pl"))
+                .body("phoneNumber", equalTo("123456789"))
+        ;
+    }
+
+    @Test
+    public void should_get_client_by_email() {
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("email", "jan.kowalski@poczta.pl");
+
+        given()
+                .header("Accept-Encoding", "application/json")
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .body(requestBody.toJSONString())
+                .log().all()
+                .when().post("/clients/get-by-email")
+                .then().log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id", notNullValue())
+                .body("id", equalTo(1))
+                .body("name", equalTo("Jan"))
+                .body("surname", equalTo("Kowalski"))
+                .body("email", equalTo("jan.kowalski@poczta.pl"))
+                .body("phoneNumber", equalTo("123456789"))
+        ;
+    }
+
+    @Test
+    public void should_create_and_delete_client() {
+        Long id = should_create_client();
+        should_delete_client(id);
+    }
+
+    private Long should_create_client() {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("name", "Ewa");
+        requestBody.put("surname", "Nowak");
+        requestBody.put("email", "ewa.nowak@poczta.pl");
+        requestBody.put("phoneNumber", "618222222");
+
+        Long id = given()
+                .header("Accept-Encoding", "application/json")
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .body(requestBody.toJSONString())
+                .log().all()
+                .when().post("/clients")
+                .then().log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id", notNullValue())
+                .body("name", equalTo("Ewa"))
+                .body("surname", equalTo("Nowak"))
+                .body("email", equalTo("ewa.nowak@poczta.pl"))
+                .body("phoneNumber", equalTo("618222222"))
+                .extract().jsonPath().getLong("id")
+        ;
+        return id;
+    }
+
+    private void should_delete_client(Long id) {
+
+        given()
+                .header("Accept-Encoding", "application/json")
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .log().all()
+                .when().delete("/clients/{id}", id)
+                .then().log().all()
+                .statusCode(200)
+        ;
+    }
+
+    @Test
+    public void should_update_client() {
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("id", 1);
+        requestBody.put("name", "Jan");
+        requestBody.put("surname", "Kowal");
+        requestBody.put("email", "jan.kowal@poczta.pl");
+        requestBody.put("phoneNumber", "123456798");
+
+        given()
+                .header("Accept-Encoding", "application/json")
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .body(requestBody.toJSONString())
+                .log().all()
+                .when().put("/clients")
+                .then().log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("id", notNullValue())
+                .body("name", equalTo("Jan"))
+                .body("surname", equalTo("Kowal"))
+                .body("email", equalTo("jan.kowal@poczta.pl"))
+                .body("phoneNumber", equalTo("123456798"))
+        ;
+        // przywrócenie
+        JSONObject requestBody1 = new JSONObject();
+        requestBody1.put("id", 1);
+        requestBody1.put("name", "Jan");
+        requestBody1.put("surname", "Kowalski");
+        requestBody1.put("email", "jan.kowalski@poczta.pl");
+        requestBody1.put("phoneNumber", "123456789");
+        given()
+                .header("Accept-Encoding", "application/json")
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .body(requestBody1.toJSONString())
+                .log().all()
+                .when().put("/clients")
+                .then().log().all()
+                .statusCode(200)
+        ;
+
+    }
+
+
+
+
+}
+
